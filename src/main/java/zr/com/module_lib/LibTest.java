cd6c85a2213ade01103741343747b3708b8530fe
yushiwo <zhengrui@otcbtc.com>
2019-03-11 11:45:21 +0800

    init proj

---
+++ src/main/java/zr/com/module_lib/LibTest.java
@@ -0,0 +1,15 @@
+package zr.com.module_lib;
+
+/**
+ * @author : yushiwo
+ * @e-mail : zhengrui@otcbtc.com
+ * @time : 2019/03/11
+ * @desc :
+ */
+public class LibTest {
+
+    public static String getLibStr() {
+        return "123";
+    }
+
+}
